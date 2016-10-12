0eabcb07feec9db36d4cb2ed372aa4e2a0b6429a
Csaba Koncz <csaba.koncz@ge.com>
2016-10-12 13:32:12 +0200

    Initial working version

---
+++ test.ts
@@ -0,0 +1,9 @@
+var foo = 123;
+foo = 'Hello World'.length;
+function a(p: string) {
+  p.length;
+}
+console.log(foo);
+var p = document.createElement('h1')
+p.innerHTML = "TS!"
+document.body.appendChild(p);
