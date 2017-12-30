392aeb1ac11e6466a1d1623a06899f7462352121
gyerekek <gyerekek>
2017-12-30 16:12:04 +0100

    WIP: Othello

---
+++ othello.ts
@@ -0,0 +1,138 @@
+var tableC = document.getElementById('table')
+var whosTurnC = document.getElementById('whosTurn')
+
+var table = []
+
+for (var i = 0; i < 8; i++){
+  var row = [];
+  var rowC = document.createElement('div')
+  rowC.classList.add('row')
+  tableC.appendChild(rowC)
+
+  table.push(row)
+
+  for (var j = 0; j < 8; j++){
+    var div = document.createElement('div')
+    div.classList.add('square')
+    var stone = document.createElement('div')
+    stone.classList.add('stone');
+    div.appendChild(stone)
+
+    rowC.appendChild(div)
+    var square = { i: i, j: j, div: stone }; 
+    createOnclick(square);
+    row.push(square)
+  }
+}
+
+var black = 'black'
+var white = 'white'
+var whosTurn;
+
+//put the two black stones:
+nextPlayer()
+toggle(table[3][3])
+toggle(table[4][4])
+
+//now the two whites
+nextPlayer()
+toggle(table[3][4])
+toggle(table[4][3])
+
+//now let the black begin:
+nextPlayer()
+
+var whites = 2;
+var blacks = 2;
+
+function nextTurn() {
+  return whosTurn == black ? white : black;
+}
+
+function nextPlayer() {
+  whosTurn = nextTurn();
+  whosTurnC.setAttribute('who', whosTurn)
+}
+
+function createOnclick(square) {
+  var div = square.div;
+  div.onclick = function() {
+    if (validClick(square)) {
+      toggle(square)
+      nextPlayer()
+    }
+  }
+}
+
+function validClick(square) {
+  if(square.color){
+    return false;
+  }
+  var captured=capture(square,false)
+  return captured>0;
+}
+
+function capture(fromSquare, dryRun: boolean) {
+  var count = 0;
+  for (var i = -1; i <= 1; i++){
+    for (var j = -1; j <= 1; j++){
+      if (i == 0 && j==0) {
+        continue;
+      }
+      count += captureDir(fromSquare, {i:i, j:j}, dryRun )
+    }
+  }
+  return count;
+}
+
+function captureDir(fromSquare, dir, dryRun: boolean) {
+  var captured = captureDir0(fromSquare, dir, true);
+  if (captured > 0 && !dryRun) {
+    captureDir0(fromSquare, dir, false)
+  }
+  return captured;
+}
+
+function captureDir0(fromSquare, dir, dryRun: boolean) {
+  var captured = 0;
+  var next;
+  var ni = fromSquare.i
+  var nj = fromSquare.j
+
+  while(true) {
+
+    ni += dir.i;
+    nj += dir.j;
+
+    var valid = validPos(ni) && validPos(nj);
+    if (!valid) {
+      return 0; //out of the table
+    }
+
+    var square = table[ni][nj];
+    if (!square.color) {
+      return 0; //empty field
+    }
+
+    if (square.color==whosTurn) {
+      return captured; //our color
+    }
+    else {
+      captured++;
+      if (!dryRun) {
+        toggle(square)
+      }
+    }
+  }
+}
+
+function toggle(square) {
+  square.color = whosTurn;
+  square.div.setAttribute('color',whosTurn)
+}
+
+function validPos(i: number) {
+  return i >= 0 && i < 8;
+}
+
+
